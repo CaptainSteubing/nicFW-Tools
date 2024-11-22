76c32ab0b2a2de5ba13c6bb3bc0edfeb645a1651
CaptainSteubing <71025755+CaptainSteubing@users.noreply.github.com>
2024-11-21 21:31:51 -0500

    Extract firmware version from binary file. Thanks Kelvin Hill.
    Thanks Kelvin Hill for the idea & commands to extract the firmware version from the binary file.

--- nicFW-nightly.sh
+++ nicFW-nightly.sh
@@ -9,14 +9,40 @@
 # DATETIME="${DATETIME^^}"
 
 DATETIME=$(date '+%d-%b-%Y-%I-%M-%p')
-FILENAME=nicFW_Nightly-$DATETIME.bin
+FILENAME=nicFW-Nightly-$DATETIME.bin
 
 # Get the latest, with a bit of magic to make sure it's not a cached version.
 curl --insecure -o $FILENAME https://nicsure.co.uk/h3/firmware.bin?_=$(date +%s)
+
+# Do magic to get the nicFW version from the downloaded file
+# Thanks Kelvin Hill!
+#
+OFFSET=$(grep -a -b -o "nicFW" "$FILENAME" | cut -d: -f1)
+# Check if the string was found
+if [ -z "$OFFSET" ]; then
+  echo "Error: String 'nicFW' not found in the file."	
+  exit 1
+else
+  OFFSET=$(( $OFFSET + 5 ))
+  echo "Success: nicFW version found at location $OFFSET" 
+fi
+
+# Read the string "nicFW" plus the next 20 characters and terminate at null character
+NICFWVERSION=$(xxd -p -s "$OFFSET" -l 20 "$FILENAME" | xxd -r -p | tr '\0' '\n' | head -n 1 )
+# Clean up the version string and create the filename 
+NICFWVERSION=$(echo "$NICFWVERSION" | sed -E -e 's/ +//g' | sed -E -e 's/\./-/' )
+NEWFILENAME=$(echo "$FILENAME" | sed -E -e 's/$DATETIME//' )
+NEWFILENAME=nicFW_Nightly-$NICFWVERSION.bin
+echo "Renaming $FILENAME to $NEWFILENAME"
+mv "$FILENAME" "$NEWFILENAME"
 
 # Grab the list of files in the directory matching "nicFW*.bin" and create an array with them.
 shopt -s nullglob
 set -- nicFW*.bin
 if [ "$#" -gt 0 ]; then
-  md5sum "$@" # Run md5sum with the list of files.
+	# Run md5sum with the list of files.
+  md5sum "$@" 
 fi
+
+# Uncomment when testing to remove the downloaded file.
+#rm $NEWFILENAME
